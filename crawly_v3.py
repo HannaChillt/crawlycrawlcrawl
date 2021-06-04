00e829fce62e198d1da56699be6c50f0473bdfe0
Johanna Mick <johanna.mick@stud.th-luebeck.de>
2021-06-04 17:18:54 +0200

    erfolgreich Stipendium angelegt

--- crawly_v3.py
+++ crawly_v3.py
@@ -12,9 +12,10 @@ class Stipendium:
         self.keywords = keywords
 
 class Termin:
-    def __init__(self, id, datum, ueberschrift):
+    def __init__(self, id, datum, info, ueberschrift):
         self.id = id
         self.datum = datum
+        self.info = info
         self.ueberschrift = ueberschrift
 
 
@@ -22,56 +23,32 @@ source = requests.get('https://gruendungsstipendium-sh.de/de/termine')
 soup = BeautifulSoup(source.content, 'lxml')
 
 search1 = soup.find_all('div', class_='gcms_text')  # ich kann nur nach content suchen, nicht nach os-content
-print(len(search1))
 
 trs = []
 for div in search1:
     for tr in div.find_all('tr'):
         trs.append(tr.text.strip().split('\n'))
 
-print(f'{trs}\n')
-
-einzeltermine = re.split(r'\n+', search1[0].text)  # einzeltermine enthaelt nur einen Eintrag
-
-
 valids = []
-''''
-for termin in einzeltermine:
-    print(termin)
-    if "Ort wird bekannt gegeben" not in termin:
-        valids.append(termin)
+for e in trs:
+    if 'Ort wird bekannt gegeben' not in e:
+        valids.append(e)
 
-for g in valids:
-    print(g)
-'''
+print(f'{valids}\n')
 
-pattern = re.compile(r"\d{2}\.\s[a-zA-ZäÄ]+\s\d{4}")
+termine1 = []
+zaehler = 0
+for v in valids:
+    termin = Termin('id', valids[zaehler][0], valids[zaehler][1], 'ueberschrift')
+    termine1.append(termin)
+    zaehler += 1
+keywords1 = ['Gründerstipendium', 'Geld']
+stipendium1 = Stipendium(1, termine1, 'https://gruendungsstipendium-sh.de/de/termine', 'https://gruendungsstipendium-sh.de/', keywords1)
+print(f'{stipendium1.info}, {stipendium1.quelle}')
+for t in stipendium1.termine:
+    print(f'{t.datum}, {t.info}')
 
-''''
-while search1:
-    for entry in search1:
-        #print(entry)
-        if pattern.findall(entry):
-            print("hello")
-            #print(search1.pop(entry.text.strip()))
-'''''
-
-
-
-#while search1:
-    #data[search1.pop().text] = search1.pop().text
-# print(f'laenge: {len(data)}')
-
-#for i in data.keys():
-    #print()
-    #print(i + ', ' + data.get(i))
-
-#termine1 = []
-#keywords1 = []
-#stipendium1 = Stipendium(1, termine1, "quelleeintragen", "infoeintragen", keywords1)
-
-
-
+# pattern = re.compile(r"\d{2}\.\s[a-zA-ZäÄ]+\s\d{4}")
 """
 an welcher Stelle sollen die Daten sortiert werden? sollen sie überhaupt sortiert werden?
 als was sollen die Daten am Ende verpackt werden? json?
